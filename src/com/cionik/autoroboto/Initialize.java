0d1c652c3f3f389aae1eb71f8b67721e90a90d2f
Omoshu <omoshulol@gmail.com>
2015-12-28 00:16:32 -0500

    Removed commented code

--- src/com/cionik/autoroboto/Initialize.java
+++ src/com/cionik/autoroboto/Initialize.java
@@ -5,7 +5,6 @@ import java.awt.event.MouseEvent;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import javax.swing.JOptionPane;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 
@@ -23,14 +22,8 @@ import com.cionik.utils.swing.MessageDialog;
 public class Initialize {
 	
 	public static void main(String[] args) {
-		//String[] options = {"Default", "System", "Lumines"};
-		//int option = JOptionPane.showOptionDialog(null, "Choose LookAndFeel", "", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null, options, options[0]);
 		try {
-			//if (option == 1) {
-				//UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-			//} else if (option == 2) {
-				UIManager.setLookAndFeel(new LuminesLookAndFeel());
-			//}
+			UIManager.setLookAndFeel(new LuminesLookAndFeel());
 		} catch (UnsupportedLookAndFeelException e) {
 			e.printStackTrace();
 		}
